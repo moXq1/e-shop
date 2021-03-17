74c05ce11f635674f4a9f9b049fcf37f6e3d2c50
moXq1 <nikitayanc@gmail.com>
2021-03-17 14:23:43 +0200

    g

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,12 @@ import { createStore } from "vuex";
 import productModule from "./modules/product.js";
 import authModule from "./modules/auth.js";
 import userModule from "./modules/user.js";
-import blogModule from "./modules/blog.js";
 
 const store = createStore({
   modules: {
     products: productModule,
     auth: authModule,
     user: userModule,
-    blog: blogModule,
   },
   getters: {
     dbUrl() {
